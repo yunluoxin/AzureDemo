f2dd28e03d7f5173e26e9f603ed7724a542fb0a9
East <zxd2@meitu.com>
2022-05-05 14:15:31 +0800

    add snapkit

--- AzureDemo/ViewController.swift
+++ AzureDemo/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    /// Title
+    private lazy var labelTitle: UILabel = {
+        let v = UILabel()
+        v.textAlignment = .center
+        v.textColor = .black
+        v.font = .systemFont(ofSize: 30, weight: .bold)
+        return v
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+        view.addSubview(labelTitle)
+        labelTitle.text = "Test for Azure"
+        labelTitle.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
-
-
 }
 
